f58e88be80d7ee0a47718f75f6a05f6b20c10e4f
pptanov <tanov.pavel20156@gmail.com>
2025-04-14 20:28:19 +0300

    xueta

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://delightful-mermaid-ed9808.netlify.app/",
 })
